450f84176718c90ef655b7bca7e4b99b7aa6d71b
Humanshu <humanshujaglan@gmail.com>
2023-10-07 20:57:53 +0530

    prepared it properly

--- remainder_quotient.c
+++ remainder_quotient.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-int main(){
-    int a,b,c;
+int main()
+{
+    int a, b, c;
     printf("enter the number you want to divide: ");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("enter the number you want to divide with: ");
-    scanf("%d",&b);
-    c= a/b;
+    scanf("%d", &b);
+    c = a / b;
     printf("quotient is %d\n", c);
-    c= a%b;
+    c = a % b;
     printf("remainder is %d", c);
     return 0;
 }
